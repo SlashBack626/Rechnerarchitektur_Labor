2cb7b08a97902ca8126853e822f64247659b700c
Andreas <Andreas.S.Weber@outlook.com>
2021-11-12 11:58:57 +0100

    write one digit

--- Labor2/Aufgabe1.X/main.c
+++ Labor2/Aufgabe1.X/main.c
@@ -1,27 +1,21 @@
 #include <xc.h>
 
-char state = 0; // 0=off 1=on
-char released = 1; //0=release 1=press
-
-void setup() {
-    TRISBbits.TRISB9 = 1; // set button as input
-    TRISBbits.TRISB0 = 0; // set LED as output
+void setup()
+{
+    initDisplay();
+    sevenSegWrite(0);
 }
 
-void loop() {
-    while (1) {
-        if (released && PORTBbits.RB9 == 0) { // Bit is 0 when button pressed
-            state = !state;
-            released = 0;
-        } else if (PORTBbits.RB9 == 1) {
-            released = 1;
-        }
-        LATBbits.LATB0 = state;
+void loop()
+{
+    while (1)
+    {
     }
 }
 
-void main() {
+void main()
+{
     SYSTEM_Initialize();
     setup();
-    loop();
+    // loop();
 }
